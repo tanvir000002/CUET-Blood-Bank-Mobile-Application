38351aa7d274ae834396c41e6986e088d1615a1f
tanvir000002 <u1804002@student.cuet.ac.bd>
2024-01-14 20:04:32 +0600

    dependency installed

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const userRouter = require("./routes/userRoutes");
+const profileRouter = require("./routes/profileRoutes");
+const postRouter = require("./routes/postRoutes");
+const cookieParser = require("cookie-parser");
+
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+app.use(userRouter);
+app.use(profileRouter);
+app.use(postRouter);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+
