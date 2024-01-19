add69d571185fd608bded46d4c75c3956f1b8ec0
tanvir000002 <u1804002@student.cuet.ac.bd>
2024-01-19 21:38:36 +0600

    frontend added

--- frontend/config/baseUrl.js
+++
@@ -1,1 +0,0 @@
-export const baseURL= 'http://192.168.0.108:3000'
--- frontend/constants/index.js
+++
@@ -1,5 +0,0 @@
-import theme, { COLORS, SIZES, FONTS } from './theme'
-import images from './images'
-import icons from './icons'
-
-export { icons, images, theme, COLORS, SIZES, FONTS }
